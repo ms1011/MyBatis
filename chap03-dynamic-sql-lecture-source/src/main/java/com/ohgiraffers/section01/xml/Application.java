78b6686148bab575199ae12a1bac86905617fc07
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-02-21 15:49:29 +0900

    MyBatis <if>, <where>, <set>, <trim> 사용해보기

--- chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
+++ chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
@@ -28,6 +28,7 @@ public class Application {
                     foreachSubMenu();
                     break;
                 case 4:
+                    trimSubMenu();
                     break;
                 case 9:
                     System.out.println("프로그램을 종료합니다.");
@@ -159,4 +160,103 @@ public class Application {
         return list;
     }
 
+    private static void trimSubMenu() {
+
+        Scanner sc = new Scanner(System.in);
+        MenuService menuService = new MenuService();
+        do {
+            System.out.println("======= trim 서브 메뉴 =======");
+            System.out.println("1. 검색 조건이 있는 경우 메뉴 코드로 조회. 단, 없으면 전체 조회");
+            System.out.println("2. 메뉴 혹은 카테고리로 검색. 단, 메뉴와 카테고리 둘 다 일치하는 경우도 검색하며, 검색 조건이 없는 경우 전체 검색");
+            System.out.println("3. 원하는 메뉴 정보만 수정하기");
+            System.out.println("9. 이전 메뉴로");
+            System.out.print("메뉴 번호를 입력해 주세요 : ");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1:
+                    menuService.searchMenuByCodeOrSearchAll(inputAllOrOne());
+                    break;
+                case 2:
+                    menuService.searchMenuByNameOrCategory(inputSearchCriteriaMap());
+                    break;
+                case 3:
+                    menuService.modifyMenu(inputChangeInfo());
+                    break;
+                case 9:
+                    return;
+                default:
+                    System.out.println("메뉴를 잘못 선택하였습니다.");
+                    break;
+            }
+
+        } while (true);
+    }
+
+    private static SearchCriteria inputAllOrOne() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("검색 조건을 입력하시겠습니까?(예 or 아니오) : ");
+        boolean hasSearchValue = "예".equals(sc.nextLine()) ? true : false;
+
+        SearchCriteria searchCriteria = new SearchCriteria();
+        if(hasSearchValue) {
+            System.out.print("검색할 메뉴 코드를 입력하세요 : ");
+            String menuCode = sc.nextLine();
+            searchCriteria.setCondition("menuCode");
+            searchCriteria.setValue(menuCode);
+        }
+
+        return searchCriteria;
+    }
+
+    /* 설명. SearchCriteria(condition과 value의 묶음) 개념을 이번에는 DTO가 아닌 Map으로 작성해 보자 */
+    private static Map<String, Object> inputSearchCriteriaMap() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("검색 조건을 입력하세요(category or name or both or none) : ");
+        String condition = sc.nextLine();
+
+        Map<String, Object> criteria = new HashMap<>();
+        if("category".equals(condition)) {
+            System.out.print("검색할 카테고리 코드를 입력하세요 : ");
+            int categoryValue = sc.nextInt();
+
+            criteria.put("categoryValue", categoryValue);
+        } else if("name".equals(condition)) {
+            System.out.print("검색할 이름을 입력하세요 : ");
+            String nameValue = sc.nextLine();
+
+            criteria.put("nameValue", nameValue);
+        } else if("both".equals(condition)) {
+            System.out.print("검색할 이름을 입력하세요 : ");
+            String nameValue = sc.nextLine();
+            System.out.print("검색할 카테고리 코드를 입력하세요 : ");
+            int categoryValue = sc.nextInt();
+
+            criteria.put("nameValue", nameValue);
+            criteria.put("categoryValue", categoryValue);
+        }
+
+        return criteria;
+    }
+
+    private static Map<String, Object> inputChangeInfo() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("변경할 메뉴 코드를 입력하세요 : ");
+        int menuCode = sc.nextInt();
+        System.out.print("변경할 메뉴 이름을 입력하세요 : ");
+        sc.nextLine();
+        String menuName = sc.nextLine();
+        System.out.print("변경할 판매 여부를 결정해 주세요(Y/N) : ");
+        String orderableStatus = sc.nextLine().toUpperCase();
+
+        Map<String, Object> criteria = new HashMap<>();
+        criteria.put("menuCode", menuCode);
+        criteria.put("menuName", menuName);
+        criteria.put("orderableStatus", orderableStatus);
+
+        return criteria;
+    }
 }
--- chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuMapper.java
+++ chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuMapper.java
@@ -1,6 +1,7 @@
 package com.ohgiraffers.section01.xml;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MenuMapper {
 
@@ -11,4 +12,10 @@ public interface MenuMapper {
     List<MenuDTO> searchMenuBySupCategory(SearchCriteria searchCriteria);
 
     List<MenuDTO> searchMenuByRandomMenuCode(List<Integer> randomList);
+
+    List<MenuDTO> searchMenuByCodeOrSearchAll(SearchCriteria searchCriteria);
+
+    List<MenuDTO> searchMenuByNameOrCategory(Map<String, Object> criteria);
+
+    int updateMenu(Map<String, Object> criteria);
 }
